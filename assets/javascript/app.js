dbc184d755c29fc43d7c01758de4b9c996b2a31e
ANS <36783010+answebdev@users.noreply.github.com>
2018-09-16 19:41:11 -0700

    initial search query logic added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,24 +3,35 @@ var searchTerm = "";
 // var q = "";
 // var limit = 10;
 // var rating = "";
+// As an animal is searched and its button is added, it is pushed into an array:
+animals = [];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eZC5OTS1J5sUexsnNh6tPs3HJweH7Smz&q=q&limit=10&offset=0&rating=G&lang=en";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eZC5OTS1J5sUexsnNh6tPs3HJweH7Smz&q=q&limit=10&offset=0&lang=en";
+
+// Original link with rating=G
+// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eZC5OTS1J5sUexsnNh6tPs3HJweH7Smz&q=q&limit=10&offset=0&rating=G&lang=en";
 
 function runQuery(animal, queryURL) {
-    $.ajax({ url: queryURL, method: "GET"})
-    .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-    });
+    $.ajax({ url: queryURL, method: "GET" })
+        .then(function (response) {
+
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Rating: " + response.data[i].rating + " || " + "URL: " + response.data[i].url)
+            }
+
+
+            console.log("query URL: " + queryURL);
+            console.log(response);
+        });
 }
 
-$("#searchBtn").on("click", function() {
+$("#searchBtn").on("click", function () {
     // alert("Test: Submit button has been clicked.");
     searchTerm = $("#search").val().trim();
-    console.log(searchTerm);
+    console.log("Search term: " + searchTerm);
 
     var newURL = queryURL + "&q=" + searchTerm;
-    console.log(newURL);
+    console.log("New URL: " + newURL);
 
     // Send the AJAX call to the newly assembled URL
     runQuery(10, newURL);
