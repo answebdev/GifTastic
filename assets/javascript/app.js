c03778487769d8d5a02cd4a1cb50fb6ee2d48333
ANS <36783010+answebdev@users.noreply.github.com>
2018-09-17 19:18:35 -0700

    jQuery added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,8 +15,12 @@ function runQuery(animal, queryURL) {
     $.ajax({ url: queryURL, method: "GET" })
         .then(function (response) {
 
+
             for (var i = 0; i < response.data.length; i++) {
-                console.log("Rating: " + response.data[i].rating + " || " + "URL: " + response.data[i].url)
+                console.log("Rating: " + response.data[i].rating + " || " + "URL: " + response.data[i].url);
+                $("#gifs").append("<p>Rating: " + response.data[i].rating + "</p>");
+                $("#gifs").append("<img src='" + response.data[i].images.downsized.url + "'>");
+
             }
 
             // Create and store a button
@@ -28,12 +32,13 @@ function runQuery(animal, queryURL) {
             // Append the new button to #buttonSection
             $("#buttonSection").append(newButton);
 
-
             console.log("query URL: " + queryURL);
             console.log(response);
         });
 }
 
+
+// Capture the value from the search field
 $("#searchBtn").on("click", function () {
     // alert("Test: Submit button has been clicked.");
     searchTerm = $("#search").val().trim();
@@ -45,8 +50,61 @@ $("#searchBtn").on("click", function () {
     // Send the AJAX call to the newly assembled URL
     runQuery(10, newURL);
 
+    // Push search terms (animals) into the "animals" array
+    animals.push(searchTerm);
+    console.log("ARRAY: " + animals + " || ARRAY LENGTH: " + animals.length);
+
     return false;
 });
+
+
+// var animal = $(this).attr("data-search");
+// var animalURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=eZC5OTS1J5sUexsnNh6tPs3HJweH7Smz&q=q&limit=10&offset=0&lang=en";
+
+// function animalQuery(animal, animalURL) {
+//     $.ajax({ url: animalURL, method: "GET" })
+//         .then(function (response) {
+
+//             var animal = $('<div class="gifArea">');
+//             var rating = response.data.rating;
+//             var pOne = $('<p>').text("Rating: " + rating);
+//             animal.append(pOne);
+
+//             var gif = $('<img>').attr("src", response.data.images.downsized.url);
+//             animal.append(gif);
+
+//             $("#gifs").prepend(animal);
+
+//         }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+//             for (var i = 0; i < response.data.length; i++) {
+//                 console.log("Rating: " + response.data[i].rating + " || " + "URL: " + response.data[i].url);
+//             }
+
+// $(newButton).on("click", function() {
+//     var animal = $(this).attr("data-search");
+//     var uniqueURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=eZC5OTS1J5sUexsnNh6tPs3HJweH7Smz&q=q&limit=10&offset=0&lang=en";
+//     alert("CLICKED!");
+// })
+
 
 // Grab text the user typed into the search input, add to the queryParams object
 // queryParams.q = $("#search-term")
